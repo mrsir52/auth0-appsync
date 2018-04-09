9da0d118e3872a4efc8e393c1b1d104e9bfc7b6d
Manny <coachmannytw@gmail.com>
2018-04-09 10:30:40 -0600

    appsync and auth0

---
+++ src/appsync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://l6esqttq2ngk7pyd3v7ii75pba.appsync-api.us-west-2.amazonaws.com/graphql",
+    "region": "us-west-2",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-brxalxofprdrph467cczgksjgy"
+}
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes';
+import Client from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
+import { ApolloProvider as Provider } from 'react-apollo'
+import App from './App'
+
+import config from './appsync'
+
+const client = new Client({
+    url: config.graphqlEndpoint,
+    region: config.region,
+    auth: {
+        type: config.authenticationType,
+        apiKey: config.apiKey
+    }
+})
+
+const WithProvider = () => (
+    <Provider client={client}>
+        <Rehydrated>
+            <App>
+                <Routes />
+            </App>
+        </Rehydrated>
+    </Provider>
+    
+)
+
+const Routes = makeMainRoutes();
+
+ReactDOM.render( <WithProvider/>,
+
+  document.getElementById('root')
+);
